558b7e4e1413de905866f0c3a703269ca8b6f2b3
yashhhaaa <yashadvg@gmail.com>
2023-01-17 20:50:45 +0530

    login page

---
+++ Login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center">SignUp for new user</h1>
+    </div>
+</body>
+</html>
